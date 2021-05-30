7ad6d7860afd5aa7b97ce365c3671765d685b876
Ben Jordan <bej9038@rit.edu>
2021-05-30 11:23:33 -0400

    Added non generic slider layout and save state functionality

--- EQ/Source/PluginEditor.cpp
+++ EQ/Source/PluginEditor.cpp
@@ -13,9 +13,11 @@
 EQAudioProcessorEditor::EQAudioProcessorEditor (EQAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    for (auto* comp : getComps())
+    {
+        addAndMakeVisible(comp);
+    }
+    setSize (800, 400);
 }
 
 EQAudioProcessorEditor::~EQAudioProcessorEditor()
@@ -30,11 +32,43 @@ void EQAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void EQAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    auto bounds = getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * .33);
+
+    auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * .25);
+    auto highCutArea = bounds.removeFromRight(bounds.getWidth() * .33);
+    auto peak1Area = bounds.removeFromRight(bounds.getWidth() * .5);
+    peak1Area.removeFromTop(peak1Area.getHeight() * .2);
+    auto peak2Area = bounds.removeFromRight(bounds.getWidth());
+    peak2Area.removeFromTop(peak2Area.getHeight() * .2);
+
+    lowCutFreqSlider.setBounds(lowCutArea);
+    highCutFreqSlider.setBounds(highCutArea);
+
+    peak1FreqSlider.setBounds(peak1Area.removeFromTop(peak1Area.getHeight() * .4));
+    peak1GainSlider.setBounds(peak1Area.removeFromTop(peak1Area.getHeight() * .5));
+    peak1QSlider.setBounds(peak1Area);
+
+    peak2FreqSlider.setBounds(peak2Area.removeFromTop(peak2Area.getHeight() * .4));
+    peak2GainSlider.setBounds(peak2Area.removeFromTop(peak2Area.getHeight() * .5));
+    peak2QSlider.setBounds(peak2Area);
+}
+
+std::vector<juce::Component*> EQAudioProcessorEditor::getComps()
+{
+    return
+    {
+        &peak1FreqSlider,
+        &peak1GainSlider,
+        &peak1QSlider,
+        &peak2FreqSlider,
+        &peak2GainSlider,
+        &peak2QSlider,
+        &lowCutFreqSlider,
+        &highCutFreqSlider
+    };
 }
